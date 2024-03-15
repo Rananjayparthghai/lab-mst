d297e45381f61edb1c99d8ba658a6bbe0183bee1
Rananjayparthghai <136495184+Rananjayparthghai@users.noreply.github.com>
2024-03-15 10:51:40 +0530

    Update exp001.py

--- exp001.py
+++ exp001.py
@@ -1,1 +1,10 @@
-print ("rananjay's practical")
+from flask import Flask
+
+app = Flask(name)
+
+@app.route('/')
+def hello():
+    return '\n\n\t\tKrish Jindal | 21BCS7871'
+
+if name == 'main':
+    app.run(debug=True)
